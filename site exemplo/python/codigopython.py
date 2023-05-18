c351b201256bbe38f4df883dc7decee4d601707a
Gabriel Cruz <gabri712289@gmail.com>
2023-05-18 16:09:52 -0400

    Delete codigopython.py

--- site exemplo/python/codigopython.py
+++
@@ -1,5 +0,0 @@
-resposta=input('Quem é melhor Cristiano Ronaldo ou Messi').strip().lower()
-if resposta == 'cristiano ronaldo':
-    print('Parabéns, você acertou!')
-else:
-    print('Você errou, o Cristiano Ronaldo é melhor')
